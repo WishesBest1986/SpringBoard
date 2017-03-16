9451a59b2eaabbf2366b631a41e4c157d22b0526
LiJun <lijun_211@126.com>
2017-03-16 14:45:22 +0800

    * 补充Test工程

---
+++ Example/Pods/Target Support Files/Pods-SpringBoard_Tests/Pods-SpringBoard_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SpringBoard_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SpringBoard_TestsVersionString[];
+
